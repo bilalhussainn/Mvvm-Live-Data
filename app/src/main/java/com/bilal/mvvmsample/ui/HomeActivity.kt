1e92d606cd227fc75cb9bf44b66a0092ac1c4505
Bilal Hussain <bilalhussain@augray.com>
2019-11-14 19:28:34 +0530

    marker design integrated.

---
+++ app/src/main/java/com/bilal/mvvmsample/ui/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.bilal.mvvmsample.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bilal.mvvmsample.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
